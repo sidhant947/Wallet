579a3ba3c63dc696a55db53b4e6cbe210026dafb
sidhant947 <khatkarsidhant@gmail.com>
2026-04-27 16:22:03 +0530

    FLAG_SECURE

--- android/app/src/main/kotlin/com/sidhant/wallet/MainActivity.kt
+++ android/app/src/main/kotlin/com/sidhant/wallet/MainActivity.kt
@@ -3,9 +3,17 @@ package com.sidhant.wallet
 import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugins.GeneratedPluginRegistrant 
+import io.flutter.plugins.GeneratedPluginRegistrant
+import android.view.WindowManager
+import android.os.Bundle
+
 class MainActivity: FlutterFragmentActivity() 
   {
+    override fun onCreate(savedInstanceState: Bundle?) {
+      super.onCreate(savedInstanceState)
+      window.addFlags(WindowManager.LayoutParams.FLAG_SECURE)
+    }
+
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) 
       {
         GeneratedPluginRegistrant.registerWith(flutterEngine) 
